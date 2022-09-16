b06a54024fd52a7a233f4c2f696e6f70cd4c6a5e
Sumit <sumit87.dgp@gmail.com>
2022-09-16 12:57:49 +0530

    Reverse keyword naming the urls

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("<str:month>", views.monthly_challenges)
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseNotFound
+from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
+from django.urls import reverse
+
+
+monthly_challenges = {
+    "january" : "Eat no meat for entire month",
+    "february" : "Walk for 2 hours",
+    "march" : "Do nothing"
+}
 
 # Create your views here.
 def january(request):
@@ -11,14 +19,19 @@ def january(request):
   
     return HttpResponse("Eat more meat")
 
-def monthly_challenges(request,month):
-    challenge_month = None
-    if month == "january":
-        challenge_month = " Eat in January"
-    elif month == "february":
-        challenge_month = "Eat in February"
-    elif month == "march":
-        challenge_month = "Eat in March"
-    else:
-        return HttpResponseNotFound("This value is not supported")                
-    return HttpResponse(challenge_month)
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    forward_month = months[month-1]
+    redirect_path = reverse("month-challenge",args=[forward_month])
+    return HttpResponseRedirect(redirect_path)
+
+
+
+
+def monthly_challenge(request,month):
+    try:
+        challenge_month = monthly_challenges[month]
+        return HttpResponse(challenge_month)
+    except:
+        return HttpResponseNotFound("Not supported")      
+    
