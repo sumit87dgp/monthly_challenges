5ec70b3d60e5ed8c57bb5eeaeacd36e379e1dd84
Sumit <sumit87.dgp@gmail.com>
2022-09-16 17:55:23 +0530

    tags & the for and url tags

--- challenges/views.py
+++ challenges/views.py
@@ -12,16 +12,20 @@ monthly_challenges = {
 }
 
 def index(request):
-    list_items = ""
+    #list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    return render(request, "challenges/index.html",{
+        "months": months
+    })
 
-    response_data =f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    # for month in months:
+    #     capitalized_month = month.capitalize()
+    #     month_path = reverse("month-challenge", args=[month])
+    #     list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    # response_data =f"<ul>{list_items}</ul>"
+    # return HttpResponse(response_data)
 
 # Create your views here.
 def january(request):
